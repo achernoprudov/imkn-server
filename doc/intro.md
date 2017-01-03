246150136cceacb4ff8073dc6677713f4d2896a4
Andrey Chernoprudov <dinloq@gmail.com>
2017-01-03 18:59:39 +0500

    init clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to imkn-server
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/imkn_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns imkn-server.core-test
+  (:require [clojure.test :refer :all]
+            [imkn-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
